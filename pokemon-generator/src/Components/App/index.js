9d852f2338226c3212369f376b91e87e633d85f2
lucaxue <lxue02@gmail.com>
2021-02-18 18:03:13 +0000

    comments

--- pokemon-generator/src/Components/App/index.js
+++ pokemon-generator/src/Components/App/index.js
@@ -6,16 +6,16 @@ import PokemonViewer from '../PokemonViewer';
 function App() {
   const [id, setId] = useState(5);
   function handleClick() {
-    // TODO: Set id to be random number between 1 and 151
+    //Set id to be random number between 1 and 151
     setId(1 + Math.round(Math.random() * 150));
   }
 
   return (
     <div className="app">
       <h1>Random Pokemon Generator!</h1>
-      {/* TODO: call handleClick when button clicked */}
+      {/*call handleClick when button clicked */}
       <button onClick={handleClick}>Get Random Pokemon</button>
-      {/* TODO: hand down id as a prop */}
+      {/*hand down id as a prop */}
       <PokemonViewer id={id} />
     </div>
   );
